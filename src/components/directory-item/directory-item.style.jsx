b7e8bd235486f08008016b8f1e7f9d87b6d37e5e
AbdelwahabTop <mramged936@gmail.com>
2023-02-13 21:57:10 +0200

    media querys

--- src/components/directory-item/directory-item.style.jsx
+++ src/components/directory-item/directory-item.style.jsx
@@ -34,29 +34,32 @@ export const Body = styled.div`
 `;
 
 export const DirectoryItemContainer = styled.div`
-  min-width: 30%;
-  height: 240px;
-  flex: 1 1 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  margin: 0 7.5px 15px;
-  overflow: hidden;
-  &:hover {
-    cursor: pointer;
-    & ${BackgroundImage} {
-      transform: scale(1.1);
-      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-    }
-    & ${Body} {
-      opacity: 0.9;
-    }
+min-width: 30%;
+height: 240px;
+flex: 1 1 auto;
+display: flex;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+margin: 0 7.5px 15px;
+overflow: hidden;
+&:first-child {
+  margin-right: 7.5px;
+}
+&:last-child {
+  margin-left: 7.5px;
+}
+&:hover {
+  cursor: pointer;
+  ${BackgroundImage} {
+    transform: scale(1.1);
+    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
   }
-  &:first-child {
-    margin-right: 7.5px;
+  ${Body} {
+    opacity: 0.9;
   }
-  &:last-child {
-    margin-left: 7.5px;
-  }
+}
+@media screen and (max-width: 800px) {
+  height: 200px;
+}
 `;
